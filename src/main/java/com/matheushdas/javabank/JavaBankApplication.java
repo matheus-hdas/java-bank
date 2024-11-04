06db9b819de178050b34346092b44aa1095c6738
matheus-hdas <matheussantos.betim@gmail.com>
2024-11-04 14:39:14 -0300

    first commit

---
+++ src/main/java/com/matheushdas/javabank/JavaBankApplication.java
@@ -0,0 +1,13 @@
+package com.matheushdas.javabank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matheushdas/javabank/JavaBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheushdas.javabank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
