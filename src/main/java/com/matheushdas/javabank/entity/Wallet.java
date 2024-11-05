9735d91550bd248d6c51ed2b9aeed4b5d368212f
matheus-hdas <matheussantos.betim@gmail.com>
2024-11-05 15:31:41 -0300

    fix: add fixed getters and setters for `Wallet` class

--- src/main/java/com/matheushdas/javabank/entity/Wallet.java
+++ src/main/java/com/matheushdas/javabank/entity/Wallet.java
@@ -3,6 +3,7 @@ package com.matheushdas.javabank.entity;
 import jakarta.persistence.*;
 
 import java.math.BigDecimal;
+import java.util.Objects;
 import java.util.UUID;
 
 @Entity
@@ -19,4 +20,66 @@ public class Wallet {
     private String email;
     private String name;
     private BigDecimal balance;
+
+    public Wallet() {}
+
+    public Wallet(String cpf, String email, String name, BigDecimal balance) {
+        this.cpf = cpf;
+        this.email = email;
+        this.name = name;
+        this.balance = balance;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Wallet wallet = (Wallet) o;
+        return Objects.equals(id, wallet.id) && Objects.equals(cpf, wallet.cpf) && Objects.equals(email, wallet.email) && Objects.equals(name, wallet.name) && Objects.equals(balance, wallet.balance);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, cpf, email, name, balance);
+    }
 }
